28ea7328adb3accd1185fe4a77358d05e098cd75
Vedaansh Vishwakarma <vedaanshvishwakarma896@gmail.com>
2026-06-06 17:16:55 +0530

    Update API URL to production endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = "https://task-sync-kadt.onrender.com/api/tasks";
 
 export const fetchTasks = async () => {
 
